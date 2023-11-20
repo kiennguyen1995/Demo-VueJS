1120d29849ab6a9d98368273eebb42e17d785447
kien.nguyen17031995 <kien.nguyen@dirox.net>
2023-11-20 17:59:01 +0700

    Init code

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>HomePage</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomePage"
+    }
+</script>
+
+<style scoped>
+
+</style>
